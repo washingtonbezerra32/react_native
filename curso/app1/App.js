f156cbe7bfa3dbbf92b183fcd2d04128139ab15a
Washington Divino Bezerra <washingtonbezerra25@gmail.com>
2020-01-07 23:39:21 -0300

    curso

---
+++ curso/app1/App.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+
+const gerarNumeroAletorio = () => {
+  var numero_aleatorio = Math.random();
+  numero_aleatorio = Math.floor(numero_aleatorio * 10)
+  alert(numero_aleatorio)
+}
+
+const App = () => {
+  return(
+    <View>
+      <Text>Gerador de números randômicos!!!</Text>
+      <Button title="Gerar um número randômico" onPress={gerarNumeroAletorio} />
+    </View>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
